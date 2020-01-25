2b4af9d263b0859684e1427f3a6022e36a9fc8a3
Benbop9 <34316758+Benbop9@users.noreply.github.com>
2020-01-25 18:51:30 -0500

    touch me

--- static/Javascript/createMap.js
+++ static/Javascript/createMap.js
@@ -1,8 +1,8 @@
 var map;
 
 function initMap() {
-  var startCenter = { lat: 43.652772, lng: -79.383673 };
-  var patientPosition = { lat: 43.652772, lng: -79.383673 };
+  var startCenter = { lat: 43.261, lng: -79.92198 };
+  var patientPosition = { lat: 43.261, lng: -79.92198 };
 
   map = new google.maps.Map(document.getElementById("map"), {
     center: startCenter,
@@ -12,9 +12,27 @@ function initMap() {
     maxZoom: 19
   });
 
-  //   var marker = new google.maps.Marker({
-  //     position: patientPosition,
-  //     map: map
-  //     //icon:
-  //   });
+  var marker = new google.maps.Marker({
+    position: patientPosition,
+    map: map
+    //icon:
+  });
+
+  // Define the LatLng coordinates for the polygon's path.
+  var safeCoords = [
+    { lat: 43.25, lng: -79.92198 },
+    { lat: 43.25, lng: -79.94 },
+    { lat: 43.27, lng: -79.92198 },
+    { lat: 43.27, lng: -79.9 }
+  ];
+
+  // Construct the polygon.
+  var safeArea = new google.maps.Polygon({
+    paths: safeCoords,
+    strokeColor: "#FF0000",
+    strokeWeight: 5,
+    fillColor: "#46eb34",
+    fillOpacity: 0.2
+  });
+  safeArea.setMap(map);
 }
