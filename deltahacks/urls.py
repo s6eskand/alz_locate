10fd259d4dfcc2a43b12c08f602c348410880762
Sam <sam.eskandar00@hotmail.com>
2020-01-25 22:58:09 -0500

    debugging

--- deltahacks/urls.py
+++ deltahacks/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('locate.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='main_page.html'), name='home')
 ]
