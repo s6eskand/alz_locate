a3c169699411f461a1904cbf07fbd94f34828b90
Benbop9 <34316758+Benbop9@users.noreply.github.com>
2020-01-25 21:04:54 -0500

    WooOOOo

--- static/Javascript/createMap.js
+++ static/Javascript/createMap.js
@@ -2,21 +2,34 @@ var map;
 
 function initMap() {
   var startCenter = { lat: 43.261, lng: -79.92198 };
-  var patientPosition = { lat: 43.261, lng: -79.92198 };
+  var patientPosition = { lat: 43.25, lng: -79.92198 };
+  var userPosition = { lat: 43.261, lng: -79.92198 };
 
   map = new google.maps.Map(document.getElementById("map"), {
     center: startCenter,
     zoom: 15,
     streetViewControl: false,
     disableDefaultUI: true,
-    minZoom: 14,
+    minZoom: 12,
     maxZoom: 19
   });
 
-  var marker = new google.maps.Marker({
+  var oldIcon = {
+    url: "/static/images/ElderlyMan.png",
+    scaledSize: new google.maps.Size(70, 70), // scaled size
+    origin: new google.maps.Point(0, 0) // origin
+  };
+
+  var patientMarker = new google.maps.Marker({
     position: patientPosition,
+    map: map,
+    icon: oldIcon
+  });
+  patientMarker.setAnimation(google.maps.Animation.BOUNCE);
+  //User's position
+  var userMarker = new google.maps.Marker({
+    position: userPosition,
     map: map
-    icon:{% static 'Images/ElderlyMan.png' %}
   });
 
   // Define the LatLng coordinates for the polygon's path.
