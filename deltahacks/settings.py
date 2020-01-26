c43e6d52c57941bb35a7aa5838cebb4178944ebe
Sam <sam.eskandar00@hotmail.com>
2020-01-26 11:36:29 -0500

    deploy to heroku

--- deltahacks/settings.py
+++ deltahacks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,6 +125,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'login'
-
+django_heroku.settings(locals())
