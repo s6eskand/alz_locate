a8eef6c5744ba12f2e95876ee7005a0325b75657
Sam <sam.eskandar00@hotmail.com>
2020-01-26 01:19:00 -0500

    make login

--- deltahacks/settings.py
+++ deltahacks/settings.py
@@ -124,4 +124,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-LOGIN_REDIRECT_URL = 'home/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- deltahacks/urls.py
+++ deltahacks/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from locate import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('locate.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('home/', TemplateView.as_view(template_name='main_page.html'), name='home'),
 ]
